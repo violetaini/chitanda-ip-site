7ec65dd551397553569f70842b9dd259f7444d26
ちーちゃん <140043984+violetaini@users.noreply.github.com>
2026-06-09 11:13:44 +0800

    config: move deploy settings to env files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { loadSiteEnv, siteEnvValue } from './scripts/site-env.mjs';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5174,
-    proxy: {
-      '/api': 'http://127.0.0.1:3022'
+function escapeHtml(value) {
+  return String(value)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
+
+export default defineConfig(({ mode }) => {
+  const siteEnv = loadSiteEnv(mode);
+
+  return {
+    plugins: [
+      react(),
+      {
+        name: 'site-html-config',
+        transformIndexHtml(html) {
+          return html
+            .replaceAll('__SITE_NAME__', escapeHtml(siteEnvValue(siteEnv, 'VITE_SITE_NAME')))
+            .replaceAll('__SITE_FAVICON_PATH__', escapeHtml(siteEnvValue(siteEnv, 'VITE_SITE_FAVICON_PATH')))
+            .replaceAll("'__SITE_LOCALE_STORAGE_KEY__'", JSON.stringify(siteEnvValue(siteEnv, 'VITE_LOCALE_STORAGE_KEY')));
+        },
+      },
+    ],
+    server: {
+      port: 5174,
+      proxy: {
+        '/api': 'http://127.0.0.1:3022'
+      }
     }
-  }
+  };
 });
